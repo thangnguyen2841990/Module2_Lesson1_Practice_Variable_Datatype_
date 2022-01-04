a533380b0f017ab8d57770ba6c52ef8542b766e5
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-01-04 10:19:45 +0700

    done

---
+++ src/Variable_DataType.java
@@ -0,0 +1,16 @@
+public class Variable_DataType {
+    public static void main(String[] args) {
+        int i = 10; // khai báo biến
+        float f = 20.5f; // khai báo biến f có kiểu giá trị là float  có giá trị là 20.5
+        double d = 20.5; // khai báo biến d có kiểu giá trị là double có giá trị là 20.5
+        boolean b = true; // khai bao bien b co kieu gia tri la boolean  co gia tri la true
+        char c = 'a'; // khai bao bien c co kieu la char  co gia tri la ky tu a
+        String s = "Hà Nội";  // khai báo biến s  có kiểu String là giá trị chuỗi Hà Nội
+        System.out.println("i = "+ i);
+        System.out.println("f = " + f);
+        System.out.println("d = "+d);
+        System.out.println("b ="+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+ s);
+    }
+}
